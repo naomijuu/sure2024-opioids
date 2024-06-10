97f45874d21237ee61fd86d18cfb34d1397398d4
Tasnim Rida <ridatasnim@gmail.com>
2024-06-10 12:49:58 -0400

    new

--- rida/test.R
+++ rida/test.R
@@ -1,9 +1,11 @@
+# loading libraries + data 
+
 library(tidyverse)
 prescriptions <- read_csv("https://raw.githubusercontent.com/36-SURE/36-SURE.github.io/main/data/prescriptions.csv")
+
+# changing data format 
 
 prescriptions.new <- prescriptions |> 
   select(State, OpioidFlag) |> 
   group_by(State, OpioidFlag) |> 
-  summarize(op = n())
-x = 1 
-  
+  summarize(num.claims = n())
