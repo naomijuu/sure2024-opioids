b4a59d53278016402def03e1ac6f8860d3b6a8a2
Tasnim Rida <ridatasnim@gmail.com>
2024-06-14 13:15:37 -0400

    added elbow plot

--- rida/clustering.R
+++ rida/clustering.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(maps)
 library(flexclust)
 library(extrafont)
-
+library(ggthemes)
+theme_set(theme_light())
 prescriptions <- read_csv("https://raw.githubusercontent.com/36-SURE/36-SURE.github.io/main/data/prescriptions.csv")
 
 
@@ -48,3 +49,50 @@ prescriptions.std |>
              color = state_cluster)) + 
   geom_point() +
   coord_fixed()
+
+# kmeans++ using flexclust
+
+library(flexclust)
+init_kmeanspp <- prescriptions.std |> 
+  select(std_brand, std_generic) |> 
+  kcca(k = 4, control = list(initcent = "kmeanspp"))
+
+prescriptions.std |>
+  mutate(
+    state_clusters = as.factor(init_kmeanspp@cluster)
+  ) |>
+  ggplot(aes(x = Brand, y = Generic,
+             color = state_clusters)) +
+  geom_point(size = 4) + 
+  ggthemes::scale_color_colorblind() +
+  labs(x = "Brand-Name Prescription Claims", y = "Generic Prescription Claims ", 
+       color = "State Clusters") +
+  theme(panel.background = element_blank(),
+        legend.position = "right", 
+        text = element_text(family = "Times New Roman"))
+
+# elbow plot
+
+prescriptions_kmpp <- function(k) {
+  
+  kmeans_results <- prescriptions.std |>
+    select(std_brand, std_generic) |>
+    kcca(k = k, control = list(initcent = "kmeanspp"))
+  
+  kmeans_out <- tibble(
+    clusters = k,
+    total_wss = sum(kmeans_results@clusinfo$size * 
+                      kmeans_results@clusinfo$av_dist)
+  )
+  return(kmeans_out)
+}
+
+n_clusters_search <- 2:12
+kmpp_search <- n_clusters_search |> 
+  purrr::map(prescriptions_kmpp) |> 
+  bind_rows()
+kmpp_search |> 
+  ggplot(aes(x = clusters, y = total_wss)) +
+  geom_line() + 
+  geom_point(size = 4) +
+  scale_x_continuous(breaks = n_clusters_search)
